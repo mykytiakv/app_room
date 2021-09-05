5383590fc03e204db5c7c155fa51ae3797a835e4
Taras Danyliuk <taras.danyliukk@gmail.com>
2021-09-05 22:26:13 +0300

    Fix layout issues

--- pages/js/popup-credit.js
+++ pages/js/popup-credit.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-  // $("#credit-pop-up").hide();
-  // $("#open-credit-pop-up").click(() => {
-  //   $("#credit-pop-up").show();
-  // });
-  // $(".close-credit-pop-up").click(() => {
-  //   $("#credit-pop-up").hide();
-  // });
-  // $(".bank-item").click((e) => {
-  //   $(".bank-item").removeClass("active");
-  //   $(e.target.closest(".bank-item")).toggleClass("active");
-  // });
+  $("#credit-pop-up").hide();
+  $("#open-credit-pop-up").click(() => {
+    $("#credit-pop-up").show();
+  });
+  $(".close-credit-pop-up").click(() => {
+    $("#credit-pop-up").hide();
+  });
+  $(".bank-item").click((e) => {
+    $(".bank-item").removeClass("active");
+    $(e.target.closest(".bank-item")).toggleClass("active");
+  });
 });
